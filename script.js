fcb3e263d8db8773d949e449fe9bbeebf3155ee6
John <johnpairat@gmail.com>
2024-06-05 00:22:58 +0800

    Encapsulate code into functions
    Created two functions: createGrid() askUser()

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const boxWidth = 1000;
-let boxNum = 16;
 
 const container = document.querySelector("#container");
-container.setAttribute("style", `max-width: ${boxWidth}px`)
+container.setAttribute("style", `max-width: ${boxWidth}px`);
 
-const squareSize = boxWidth/boxNum;
+function askUser () {
+    let userPrompt = Number(prompt("How many number of squares per side would you like in the grid?"));
+    while (userPrompt > 100) {
+        userPrompt = Number(prompt("Maximum of 100 squares per side! Please enter a new amount:"));
+    }
+    return userPrompt
+}
 
-for (let i=0; i<boxNum; i++) {
-    for (let j=0; j<boxNum; j++) {
-        const box = document.createElement("div");
-        box.setAttribute("style", `outline: solid black 1.5px; min-width: ${squareSize}px; height: ${squareSize}px; flex: 1`);
-        container.appendChild(box);
+
+function createGrid () {
+    const boxNum = askUser()
+    const squareSize = boxWidth/boxNum;
+    for (let i=0; i<boxNum; i++) {
+        for (let j=0; j<boxNum; j++) {
+            const box = document.createElement("div");
+            box.setAttribute("style", `outline: solid gray 1.5px; min-width: ${squareSize}px; height: ${squareSize}px; flex: 1`);
+            container.appendChild(box);
+        }
     }
 }
 
+const button = document.querySelector("#button-grid");
+button.addEventListener("click", createGrid)
